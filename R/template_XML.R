70bef096f5bb34328b2a6c8f11d1635f1e61c8b6
JoelJWilson <joel.j.wilson@student.adelaide.edu.au>
2018-01-31 03:51:48 +0000

    Initial Commit

---
+++ R/template_XML.R
@@ -0,0 +1,21 @@
+#' @title Import template XML file
+#' 
+#' @description Take a template XML file and replace sequences and dates
+#' 
+#' @param file template XML file
+#' 
+#' @export
+template_XML <- function(file){
+  template <- xmlInternalTreeParse(file) #imports xml into internal tree
+  template <- xmlRoot(template) #converts imported tree into a workable node
+  
+  data_attrs <- xmlAttrs(template[["data"]])   #attributes of the node "data"
+  data <- newXMLNode("data") #creates new node
+      xmlAttrs(data) <- data_attrs #writes attributes to new node
+      removeChildren(template,"data") #removes node "data" and all sequences
+      addChildren(template,data,at=0) #adds new node
+      
+    removeChildren(template[["run"]][["state"]][["tree"]][["trait"]],"text") #removes taxon and dates
+      
+  template
+}
