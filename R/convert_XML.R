ccb3fcceb16cbd58fd8f1876f526af68a266bf23
JoelJWilson <joel.j.wilson@student.adelaide.edu.au>
2018-02-01 01:42:14 +0000

    Added comments

--- R/convert_XML.R
+++ R/convert_XML.R
@@ -7,31 +7,37 @@
 #' @param name File name that the resulting XML file will be saved as
 #' 
 #' @export
-convert_XML <- function(file,template,name){
-  
-  # Import sequence data from specified file
-  data <- import_file(file)
-  
-  # Create a template XML file from specified file
-  template <- template_XML(template)
-  
-  #add new sequences to template
-  for (n in 1:(length(data[,2]))) {
-    sequence <- newXMLNode("sequence")
-    edit_Sequence(sequence,paste("seq_",data[n,1],sep = ""),paste(data[n,1]),"4",paste(data[n,2]))
-    addChildren(template[["data"]],sequence)
-  }
-  
-  #
-  removeChildren(template[["run"]][["state"]][["tree"]],"trait")
-  text <- (paste(data[,1],"=0,\n",sep=""))
-  text[length(text)] <- gsub(",","",text[length(text)])
-  newXMLNode("trait", text, attrs=c(id="dateTrait.t:test",spec="beast.evolution.tree.TraitSet",traitname="date"),parent=template[["run"]][["state"]][["tree"]],at=0)
-  newXMLNode("taxa",attrs=c(id='TaxonSet.test',spec='TaxonSet'),parent=template[["run"]][["state"]][["tree"]][["trait"]])
-  newXMLNode("alignment",attrs=c(idref="test"),parent=template[["run"]][["state"]][["tree"]][["trait"]][["taxa"]])
-  
-  
-  saveXML(template, name)
-  template
-  
+convert_XML <- function(file, template, name) {
+    
+    # Import sequence data from specified file
+    data <- import_file(file)
+    
+    # Create a template XML file from specified file
+    template <- template_XML(template)
+    
+    # add new sequences to template
+    for (n in 1:(length(data[, 2]))) {
+        sequence <- newXMLNode("sequence")
+        edit_Sequence(sequence, paste("seq_", data[n, 1], sep = ""), paste(data[n, 1]), "4", paste(data[n, 2]))
+        addChildren(template[["data"]], sequence)
+    }
+    
+    # Removing node with dates
+    removeChildren(template[["run"]][["state"]][["tree"]], "trait")
+    # Paste in sequence IDs with dat 0
+    text <- (paste(data[, 1], "=0,\n", sep = ""))
+    # remove comma from final ID
+    text[length(text)] <- gsub(",", "", text[length(text)])
+    # adding back in nodes and attributes that were removed
+    newXMLNode("trait", text, attrs = c(id = "dateTrait.t:test", spec = "beast.evolution.tree.TraitSet", traitname = "date"), 
+        parent = template[["run"]][["state"]][["tree"]], at = 0)
+    newXMLNode("taxa", attrs = c(id = "TaxonSet.test", spec = "TaxonSet"), parent = template[["run"]][["state"]][["tree"]][["trait"]])
+    newXMLNode("alignment", attrs = c(idref = "test"), parent = template[["run"]][["state"]][["tree"]][["trait"]][["taxa"]])
+    
+    # Save the new XML file with a name
+    saveXML(template, name)
+    
+    # Print new XML file
+    template
+    
 }
