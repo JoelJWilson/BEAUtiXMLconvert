5aabd86c50bad487f55523d7916ac544a9519d6c
JoelJWilson <joel.j.wilson@student.adelaide.edu.au>
2018-02-05 01:41:07 +0000

    Updated description of storeEvery and chainLength

--- R/convert_XML.R
+++ R/convert_XML.R
@@ -6,40 +6,40 @@
 #' @param template XML file to be used as a template.
 #' @param name File name that the resulting XML file will be saved as.
 #' @param loc File containing location data.
-#' @param chainLength Integer determining the chainlength of the MCMC run. Defaults to length of template file
-#' @param storeEvery Integer determining how often to store the results of the MCMC run. Defaults to template number
+#' @param chainLength Integer determining the chainlength of the MCMC run. Defaults to template file specifications.
+#' @param storeEvery Integer determining how often to store the results of the MCMC run. Defaults to template file specifications.
 #'
 #' @export
 convert_XML <- function(file, template, name, loc = FALSE, chainLength = FALSE, storeEvery = FALSE) {
-
+    
     # Import sequence data from specified file
     data <- import_sequences(file)
-
+    
     # Create a template XML file from specified file
     template <- template_XML(template)
-
+    
     # Function to add sequences to an XML node
     edit_Sequence <- function(sequence, ...) {
-
+        
         args <- list(...)
         id <- args[1]
         taxon <- args[2]
         totalcount <- args[3]
         value <- args[4]
-
+        
         XML::xmlAttrs(sequence)["id"] <- id
         XML::xmlAttrs(sequence)["taxon"] <- taxon
         XML::xmlAttrs(sequence)["totalcount"] <- totalcount
         XML::xmlAttrs(sequence)["value"] <- value
     }
-
+    
     # add new sequences to template
     for (n in 1:(length(data[, 2]))) {
         sequence <- XML::newXMLNode("sequence")
         edit_Sequence(sequence, paste("seq_", data[n, 1], sep = ""), paste(data[n, 1]), "4", paste(data[n, 2]))
         XML::addChildren(template[["data"]], sequence)
     }
-
+    
     # Removing node with dates
     XML::removeChildren(template[["run"]][["state"]][["tree"]], "trait")
     # Paste in sequence IDs with dat 0
@@ -47,65 +47,65 @@ convert_XML <- function(file, template, name, loc = FALSE, chainLength = FALSE, 
     # remove comma from final ID
     text[length(text)] <- gsub(",", "", text[length(text)])
     # adding back in nodes and attributes that were removed
-    XML::newXMLNode("trait", text, attrs = c(id = "dateTrait.t:test", spec = "beast.evolution.tree.TraitSet", traitname = "date"),
+    XML::newXMLNode("trait", text, attrs = c(id = "dateTrait.t:test", spec = "beast.evolution.tree.TraitSet", traitname = "date"), 
         parent = template[["run"]][["state"]][["tree"]], at = 0)
     XML::newXMLNode("taxa", attrs = c(id = "TaxonSet.test", spec = "TaxonSet"), parent = template[["run"]][["state"]][["tree"]][["trait"]])
     XML::newXMLNode("alignment", attrs = c(idref = "test"), parent = template[["run"]][["state"]][["tree"]][["trait"]][["taxa"]])
-
+    
     # Import location data
     if (loc != FALSE) {
-
+        
         # Read location data file
         loc_data <- utils::read.delim(loc)
-
+        
         # Paste location data in template XML file
         loc_text <- paste(loc_data[, 1], "=", loc[, 2], ",\n", sep = "", collapse = "")
         loc_text[length(loc_text)] <- gsub(",", "", loc_text[length(loc_text)])
         template[["run"]][["distribution"]][[2]][[2]][["data"]][["traitSet"]][["text"]] <- loc_text
-
+        
         # Edit XML attributes
         loc_names <- unique(sort(loc_data[, 2]))
         loc_count <- length(loc_names)
-        codeMap_attr <- paste(paste(loc_names, "=", 1:loc_count - 1, ",", collapse = "", sep = ""), "? =", paste(1:loc_count -
+        codeMap_attr <- paste(paste(loc_names, "=", 1:loc_count - 1, ",", collapse = "", sep = ""), "? =", paste(1:loc_count - 
             1, collapse = " "))
         XML::xmlAttrs(template[["run"]][["distribution"]][[2]][[2]][["data"]][["userDataType"]])["codeMap"] <- codeMap_attr
         XML::xmlAttrs(template[["run"]][["distribution"]][[2]][[2]][["data"]][["userDataType"]])["states"] <- loc_count
     }
     # change MCMC chainlength
-    if (chainLength != FALSE){
-      XML::xmlAttrs(template[["run"]])["chanLength"] <- chainLength
+    if (chainLength != FALSE) {
+        XML::xmlAttrs(template[["run"]])["chanLength"] <- chainLength
     }
     # Change store every
-    if (storeEvery != FALSE){
-      XML::xmlAttrs(template[["run"]][["state"]])["storeEvery"] <- storeEvery
+    if (storeEvery != FALSE) {
+        XML::xmlAttrs(template[["run"]][["state"]])["storeEvery"] <- storeEvery
     }
-
-
+    
+    
     ## Replace Names-----------------------------------------------------------------------------------------------
-
+    
     # Extract the file name from the template
     template_name <- tools::file_path_sans_ext(gsub(".*/", "", template))
-
+    
     # Extract the alignment name from the template
     alignment_name <- XML::xmlAttrs(template[["data"]])["id"]
-
+    
     # Extract file name from saved filepath
     file_name <- tools::file_path_sans_ext(gsub(".*/", "", name))
-
+    
     # Extract the file name from the sequence import
     import_name <- tools::file_path_sans_ext(gsub(".*/", "", file))
-
+    
     # Convert to character string
     XML_doc <- XML::toString.XMLNode(template)
-
+    
     # Replace template name with new
     XML_doc <- gsub(template_name, file_name, XML_doc)
-
+    
     # Replace old aligment name with new
     XML_doc <- gsub(alignment_name, import_name, XML_doc)
-
+    
     ## Save file-----------------------------------------------------------------------------------------------------
     ## Save the new XML file with a name
     write(XML_doc, file = name)
-
+    
 }
--- R/import_sequences.R
+++ R/import_sequences.R
@@ -6,109 +6,109 @@
 #'
 #' @export
 import_sequences <- function(file) {
-
+    
     ## Iport .fasta file ##
     import_fasta <- function(file) {
-
+        
         # Read File
         fasta_file <- readLines(file)
-
+        
         # Find read ID using > tag
         names_fasta <- fasta_file[grep(pattern = ">", x = fasta_file)]
-
+        
         # Remove tag from names
         names_fasta <- gsub(">", "", names_fasta)
-
+        
         # Remove '' from names
         names_fasta <- names_fasta <- gsub("'", "", names_fasta)
-
-
+        
+        
         # Create empty list to add sequences to
         fasta_sequence <- list()
-
+        
         # Add sequences to list
         for (n in 1:(length(names_fasta) - 1)) {
-            sequence <- fasta_file[(grep(pattern = ">", x = fasta_file)[n] + 1):(grep(pattern = ">", x = fasta_file)[n +
+            sequence <- fasta_file[(grep(pattern = ">", x = fasta_file)[n] + 1):(grep(pattern = ">", x = fasta_file)[n + 
                 1] - 1)]
             sequence <- paste(sequence, collapse = "")
-
+            
             fasta_sequence <- rbind(fasta_sequence, sequence)
         }
-
+        
         sequence <- fasta_file[(grep(pattern = ">", x = fasta_file)[length(names_fasta)] + 1):length(fasta_file)]
         sequence <- paste(sequence, collapse = "")
         fasta_sequence <- rbind(fasta_sequence, sequence)
-
+        
         # Combine seqences and ID to a data frame
         fasta_data <- suppressWarnings(cbind.data.frame(names_fasta, fasta_sequence))
-
+        
         # Return data frame
         fasta_data
     }
-
+    
     ## Import .nex file ##
     import_nex <- function(file) {
-
+        
         # Read file and comvert to dataframe
         dataframe <- as.data.frame(scan(file, character(), quiet = TRUE))
-
+        
         # Find where the sequences start
         start.line <- grep("matrix", dataframe[, 1], ignore.case = TRUE) + 1
-
+        
         # Find where the sequences end
         end.line <- grep("end", dataframe[start.line:length(dataframe[, 1]), 1], ignore.case = TRUE)[1] - 3 + start.line
-
+        
         # Length of the sequence data
         l <- end.line - start.line
-
+        
         # Find the number of sequences
-        n.tax <- utils::type.convert(gsub(";", "", gsub("ntax=", "", dataframe[grep("ntax=", dataframe[, 1], ignore.case = TRUE),
+        n.tax <- utils::type.convert(gsub(";", "", gsub("ntax=", "", dataframe[grep("ntax=", dataframe[, 1], ignore.case = TRUE), 
             ], ignore.case = TRUE)))
-
+        
         # ID of the sequences
         name <- dataframe[(1:(n.tax)) * 2 + start.line - 2, ]
-
+        
         # Create a list of all the sequences
         sequence <- list()
         for (n in 1:n.tax) {
-            sequence_ <- paste(dataframe[(1:((l + 1)/(2 * n.tax))) * 2 * n.tax - (2 * n.tax - start.line + 1 - 2 *
+            sequence_ <- paste(dataframe[(1:((l + 1)/(2 * n.tax))) * 2 * n.tax - (2 * n.tax - start.line + 1 - 2 * 
                 n), ], collapse = "")
             sequence <- rbind(sequence, sequence_)
         }
-
+        
         # Combine ID and sequences into a data frame
         nex_data <- suppressWarnings(cbind.data.frame(name, sequence))
-
+        
         # Return data frame
         nex_data
     }
-
+    
     ## Import .phy file ##
     import_phy <- function(file) {
-
+        
         # Read file
         phy_data <- as.data.frame(scan(file, character()))
-
+        
         # Find the number of sequences
         n.tax <- phy_data[1, 1]
-
+        
         # Find how many chracters each sequence is
         n.char <- phy_data[2, 1]
-
+        
         # Create list of ID
         phy_names <- phy_data[((1:n.tax) * 2 + 1), 1]
-
+        
         # Create list of sequences
         phy_sequence <- phy_data[((1:n.tax) * 2 + 2), 1]
-
+        
         # Combine ID and sequences into a dataframe
         phy_data <- suppressWarnings(cbind.data.frame(phy_names, phy_sequence))
-
+        
         # Return dataframe
         phy_data
-
+        
     }
-
+    
     # Find the apporpraite function for the filetype
     if (grepl("*.nex$", file) == TRUE) {
         data <- import_nex(file)
@@ -123,6 +123,6 @@ import_sequences <- function(file) {
             }
         }
     }
-
+    
     data
 }
