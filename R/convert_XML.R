7b2b89a2a4a25a1f05c1d787fe10e88c3c3a7515
JoelJWilson <joel.j.wilson@student.adelaide.edu.au>
2018-02-22 22:21:57 +0000

    Removed dependency on import_sequences

--- R/convert_XML.R
+++ R/convert_XML.R
@@ -22,6 +22,82 @@
 #' @export
 convert_XML <- function(input, datatype, template, file, dates = NULL, location = NULL, chainLength = NULL, storeEvery = NULL) {
     
+    import_sequences <- function(file) {
+        
+        ## Import .nex file ##
+        import_nex <- function(file) {
+            
+            # Read file and comvert to dataframe
+            dataframe <- as.data.frame(scan(file, character(), quiet = TRUE))
+            
+            # Find where the sequences start
+            start.line <- grep("matrix", dataframe[, 1], ignore.case = TRUE) + 1
+            
+            # Find where the sequences end
+            end.line <- grep("end", dataframe[start.line:length(dataframe[, 1]), 1], ignore.case = TRUE)[1] - 3 + 
+                start.line
+            
+            # Length of the sequence data
+            l <- end.line - start.line
+            
+            # Find the number of sequences
+            n.tax <- utils::type.convert(gsub(";", "", gsub("ntax=", "", dataframe[grep("ntax=", dataframe[, 1], 
+                ignore.case = TRUE), ], ignore.case = TRUE)))
+            
+            # ID of the sequences
+            seq.name <- dataframe[(1:(n.tax)) * 2 + start.line - 2, ]
+            
+            # Create a list of all the sequences
+            seq.text <- list()
+            for (n in 1:n.tax) {
+                sequence <- paste(dataframe[(1:((l + 1)/(2 * n.tax))) * 2 * n.tax - (2 * n.tax - start.line + 1 - 
+                  2 * n), ], collapse = "")
+                seq.text <- rbind(seq.text, sequence)
+            }
+            
+            # Combine ID and sequences into a data frame
+            nex_data <- suppressWarnings(cbind.data.frame(seq.name, seq.text))
+            
+            # Return data frame
+            nex_data
+        }
+        
+        # import fasta files
+        import_fasta <- function(file) {
+            phylotools::read.fasta(file, clean_name = FALSE)
+        }
+        # import phylic files
+        import_phy <- function(file) {
+            phylotools::read.phylip(file, clean_name = FALSE)
+        }
+        
+        # Find the apporpraite function for the filetype
+        if (grepl("*.nex$", file) == TRUE) {
+            data <- import_nex(file)
+        } else {
+            if (grepl("*.fasta$", file) == TRUE) {
+                data <- import_fasta(file)
+            } else {
+                if (grepl("*.phy$", file) == TRUE) {
+                  data <- import_phy(file)
+                } else {
+                  stop("error")
+                }
+            }
+        }
+        
+        if (length(unique(nchar(as.vector(data[, 2])))) != 1) 
+            stop("Inconsistent sequence lengths")
+        
+        # remove apostrophes from names
+        data[, 1] <- gsub("'", "", data[, 1])
+        
+        # remove NA from end of line
+        data[, 2] <- gsub("NA$", "", data[, 2])
+        
+        data
+    }
+    
     # Import sequence data from specified file
     data <- import_sequences(input)
     
@@ -122,8 +198,8 @@ convert_XML <- function(input, datatype, template, file, dates = NULL, location 
             # Edit XML attributes
             loc_names <- unique(sort(loc_data[, 2]))
             loc_count <- length(loc_names)
-            codeMap_attr <- paste(paste(loc_names, "=", 1:loc_count - 1, ",", collapse = "", sep = ""), "? =", 
-                paste(1:loc_count - 1, collapse = " "))
+            codeMap_attr <- paste(paste(loc_names, "=", 1:loc_count - 1, ",", collapse = "", sep = ""), "? =", paste(1:loc_count - 
+                1, collapse = " "))
             XML::xmlAttrs(XML_template[["run"]][["distribution"]][[2]][[2]][["data"]][["userDataType"]])["codeMap"] <- codeMap_attr
             XML::xmlAttrs(XML_template[["run"]][["distribution"]][[2]][[2]][["data"]][["userDataType"]])["states"] <- loc_count
         }
@@ -151,8 +227,7 @@ convert_XML <- function(input, datatype, template, file, dates = NULL, location 
     # Change .log output name
     XML_doc <- gsub("fileName=(.{,30})\\.log", paste("fileName=\\\"", file_name, ".log", sep = ""), XML_doc)
     
-    ## Save
-    ## file-----------------------------------------------------------------------------------------------------
+    ## Save file-----------------------------------------------------------------------------------------------------
     write(XML_doc, file = file)
     
 }
